38490fac1e53468d5713aef48b61a18197222bb8
7sumona02 <hawkingsumona8@gmail.com>
2024-07-22 19:58:23 +0530

    added all

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        veryDarkBlue: '#1B262C',
+        darkBlue: '#0F4C75',
+        lightBlue: '#3282B8',
+        veryLightBlue: '#BBE1FA',
+      },
+      fontFamily: {
+        sans: ['Poppins', 'sans-serif']
+      },
+      spacing: {
+
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
